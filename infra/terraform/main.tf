c4ab8979eb74b11620383a4beb3b70c7ea6d7a3f
fcramos296 <fernando.camargo5@hotmail.com>
2026-09-11 07:52:26 -0300

    Add AWS account and naming locals

---
+++ infra/terraform/main.tf
@@ -0,0 +1,15 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  name_prefix = "${var.project_name}-${var.environment}"
+  runner_image = "${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}"
+
+  github_subjects = [
+    for ref in var.github_allowed_refs :
+    "repo:${var.github_repository}:ref:${ref}"
+  ]
+}
